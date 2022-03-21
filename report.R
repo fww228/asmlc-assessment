fcbc0465c75a461a29012d7c7603f31087d1d1c7
枫歪歪 <dev.nie@outlook.com>
2022-03-21 22:27:22 +0000

    Update

--- report.R
+++ report.R
@@ -1,12 +1,12 @@
 install.packages("infotheo")
-install.packages("pROC")
 install.packages("caret")
-install.packages("mlr3verse")
 install.packages("tidyverse")
 install.packages("ggplot2")
 install.packages("GGally")
 install.packages("data.table")
 install.packages("mlr3verse")
+install.packages("mlr3learners")
+install.packages("mlr3proba")
 
 library("tidyverse")
 library("ggplot2")
@@ -15,6 +15,8 @@ library("infotheo")
 library("caret")
 library("data.table")
 library("mlr3verse")
+library("mlr3learners")
+library("mlr3proba")
 
 hotels0 = readr::read_csv("https://www.louisaslett.com/Courses/MISCADA/hotels.csv")
 
@@ -26,6 +28,7 @@ ggplot(hotels0 %>% filter(adr < 4000) %>% mutate(total_nights = stays_in_weekend
 DataExplorer::plot_boxplot(hotels0, by = "is_canceled", ncol = 3)
 DataExplorer::plot_bar(hotels0, by = "is_canceled", ncol = 2)
 
+# Data cleaning
 hotels1 <- hotels0 %>%
   mutate(total_nights = stays_in_weekend_nights+stays_in_week_nights) %>% 
   mutate(kids = case_when(children + babies > 0 ~ "kids", TRUE ~ "none")) %>%
@@ -47,49 +50,93 @@ ggpairs(hotels2 %>% select(kids, lead_time, is_repeated_guest, previous_cancella
                            booking_changes, days_in_waiting_list, total_of_special_requests, 
                            total_nights, room_type, is_canceled), aes(color = kids))
 
+# Data cleaning
 hotels3 <- hotels2 %>% 
   select(-hotel, -meal, -market_segment, -distribution_channel, -kids, -parking, -adr, -total_nights)
-
-# Figure 8
-hotels4 <- hotels3 %>% group_by(days_in_waiting_list) %>% count(days_in_waiting_list)
-hotels4 %>% mutate(prop = n/119390)
-
 hotels4 <- hotels3 %>% select(-days_in_waiting_list)
 
-# Figure 9
+# Figure 8
 hotels4 %>% group_by(lead_time) %>% summarise(prop = mean(is_canceled)) %>% ggplot(aes(x=lead_time, y=prop))+
   geom_bar(stat='identity', width=2)
 
+# Modeling
 equal_freq_data <- discretize(hotels4$lead_time, 'equalfreq', 4)
 sort_data <- hotels4$lead_time[order(hotels4$lead_time)]
 hotels <- hotels4 %>%
   mutate(lead_time = case_when(lead_time>160 ~ 4, lead_time>69 & lead_time<=160 ~ 3, 
                                lead_time>18 & lead_time<=69 ~ 2, TRUE ~ 1))
 
+# Simple logistic regression
 fit.lr <- glm(as.factor(is_canceled) ~ ., binomial, hotels)
-summary(fit.lr)
 
-# Figure 10
+# Figure 9
 pred.lr <- predict(fit.lr, hotels, type = "response")
 ggplot(data.frame(x = pred.lr), aes(x = x)) + geom_histogram()
 
 # Result of simple logistic regression
-conf.mat <- table(`true cancel` = hotels$is_canceled, `predict cancel` = pred.lr > 0.5)
+conf.mat <- table(`canceled` = hotels$is_canceled, `predict cancel` = pred.lr > 0.5)
 conf.mat
 conf.mat/rowSums(conf.mat)*100
 
-hotels <- hotels %>% select(-customer_type)
+# Model improvements
+## Add hyperparameter epsilon
+fit.lr <- glm(as.factor(is_canceled) ~ ., binomial, hotels, epsilon = 1e-5)
+pred.lr <- predict(fit.lr, hotels, type = "response")
+conf.mat <- table(`canceled` = hotels$is_canceled, `predict cancel` = pred.lr > 0.5)
+# Figure 12 and 13
+conf.mat
+conf.mat/rowSums(conf.mat)*100
+
+## Cross validation
+hotels <- hotels %>% select(-customer_type, -adults)
 hotels$deposit_type <- factor(hotels$deposit_type, levels=c("No Deposit", "Non Refund", "Refundable"), ordered = TRUE)
-
-# Cross-validation
-lb <- lrn("classif.featureless", predict_type = "prob")
-lc <- lrn("classif.rpart", predict_type = "prob")
-
 hotels$is_canceled <- factor(hotels$is_canceled, levels=c("0", "1"), ordered = TRUE)
+hotels$lead_time <- factor(hotels$lead_time)
+hotels$is_repeated_guest <- factor(hotels$is_repeated_guest)
+hotels$previous_cancellations <- factor(hotels$previous_cancellations)
+hotels$previous_bookings_not_canceled <- factor(hotels$previous_bookings_not_canceled)
+hotels$booking_changes <- factor(hotels$booking_changes)
+hotels$total_of_special_requests <- factor(hotels$total_of_special_requests)
+
 set.seed(26)
-hotels_task <- TaskClassif$new(id = "is_canceled", backend = hotels, target = "is_canceled", positive = "1")
+hotels_task <- TaskClassif$new(id = "hotels", backend = na.omit(hotels), target = "is_canceled")
 cv5 <- rsmp("cv", folds = 5)
 cv5$instantiate(hotels_task)
 
-rb <- resample(hotels_task, lb, cv5, store_models = TRUE)
-rc <- resample(hotels_task, lc, cv5, store_models = TRUE)
+lrn_cart_cv <- lrn("classif.rpart", predict_type = "prob", xval = 10)
+res_cart_cv <- resample(hotels_task, lrn_cart_cv, cv5, store_models = TRUE)
+# Figure 10
+rpart::plotcp(res_cart_cv$learners[[5]]$model)
+
+# Diﬀerent models and super learner
+lrnsp_log_reg <- lrn("classif.log_reg", predict_type = "prob", id = "super")
+pl_missing <- po("fixfactors") %>>%
+  po("removeconstants") %>>%
+  po("imputesample", affect_columns = selector_type(c("ordered", "factor"))) %>>%
+  po("imputemean")
+pl_factor <- po("encode")
+spr_lrn <- gunion(list(
+  gunion(list(
+    po("learner_cv", lrn_baseline),
+    po("learner_cv", lrn_cart),
+    po("learner_cv", lrn_cart_cp)
+  )),
+  pl_missing %>>%
+    gunion(list(
+      po("learner_cv", lrn_ranger),
+      po("learner_cv", lrn_log_reg),
+      po("nop")
+    )),
+  pl_factor %>>%
+    po("learner_cv", lrn_xgboost)
+)) %>>%
+  po("featureunion") %>>%
+  po(lrnsp_log_reg)
+# Figure 11
+spr_lrn$plot()
+res_spr <- resample(hotels_task, spr_lrn, cv5, store_models = TRUE)
+res_spr$aggregate(list(msr("classif.ce"),
+                       msr("classif.acc"),
+                       msr("classif.fpr"),
+                       msr("classif.fnr")))
+
